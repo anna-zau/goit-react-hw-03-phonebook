aeead30e9dd7df8286701ba22942ba6cc86f9ecb
anna-zau <annazauholnikova555@gmail.com>
2022-12-10 00:17:57 +0200

    added contacts to local storage
    Added local storage saving logic

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const DivSection = styled.div`
+  display: block;
+  margin: 0 auto;
+  padding: 30px 60px;
+  width: 500px;
+  background: linear-gradient(to right, #fbd3e9, #bb377d);
+  border-radius: 15px;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-top: 0;
+`;
+
+export const TitleTwo = styled.h2`
+  text-align: center;
+`;
